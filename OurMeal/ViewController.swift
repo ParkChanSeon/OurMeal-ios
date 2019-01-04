29845b964316a61018f97484455b8b22ac906887
Chan-Seon Park <chan@chan-seonui-macbook-pro.local>
2019-01-04 09:36:19 +0900

    Initial Commit

---
+++ OurMeal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OurMeal
+//
+//  Created by Chan-Seon Park on 2019. 1. 4..
+//  Copyright © 2019년 OurMeal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
